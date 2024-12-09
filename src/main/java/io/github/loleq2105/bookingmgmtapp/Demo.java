c902353450ce7cb3113079fa12e41a2828a1df30
loleq2105 <emkarol21@gmail.com>
2024-12-09 07:08:47 +0100

    Rename Main to Demo

--- src/main/java/io/github/loleq2105/bookingmgmtapp/Demo.java
+++ src/main/java/io/github/loleq2105/bookingmgmtapp/Demo.java
@@ -10,7 +10,7 @@ import java.io.InputStreamReader;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
-public class Main {
+public class Demo {
 
 
     public static void main(String[] args) {
